f320b58bc10ff33024a58e858b29e58f310a18d6
marcin.tomaszek <marcin.tomaszek@microsoft.wsei.edu.pl>
2024-10-08 18:15:35 +0200

    calculator

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -18,6 +18,56 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult About()
+    {
+        return View();
+    }
+    
+    public IActionResult Calculator(Operator? op, double? x, double? y)
+    {
+        // var op = Request.Query["op"];
+        // var x = double.Parse(Request.Query["x"]);
+        // var y = double.Parse(Request.Query["y"]);
+        if (x is null || y is null)
+        {
+            ViewBag.ErrorMessage = "Wrong number format in parimeter x or y";
+            if (x is  null && op != Operator.sin)
+            {
+                return View("CalculatorError");
+            }
+        }
+
+        if (op is null)
+        {
+            ViewBag.ErrorMessage = "Unknown operator";
+            return View("CalculatorError");
+        }
+            
+        switch (op)
+        {
+            case Operator.add:
+                ViewBag.Result = x + y;
+                break;
+            case Operator.sub:
+                ViewBag.Result = x - y;
+                break;
+            case Operator.mul:
+                ViewBag.Result = x * y;
+                break;
+            case Operator.div:
+                ViewBag.Result = x / y;
+                break;
+            case Operator.pow:
+                ViewBag.Result = Math.Pow((double)x,(double)y);
+                break;
+            case Operator.sin:
+                ViewBag.Result = Math.Sin((double)x);
+                break;
+        }
+            
+        return View();
+    }
+
     public IActionResult Privacy()
     {
         return View();
@@ -27,5 +77,10 @@ public class HomeController : Controller
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    public enum Operator
+    {
+        add,sub,mul,div,pow,sin
     }
 }
