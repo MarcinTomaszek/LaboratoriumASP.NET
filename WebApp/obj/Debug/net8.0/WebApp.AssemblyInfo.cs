9b9a42f29e383ba634bddd1d9561dde9b75e6214
BlueJack14 <bluejack1414@gmail.com>
2024-10-29 17:07:31 +0100

    init lab4

--- WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e64cf64b437dba46483afee634630c120f59534e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
