552524ba48bd09e9548de8c2176a80020f4cbc94
marcin.tomaszek <marcin.tomaszek@microsoft.wsei.edu.pl>
2024-11-12 17:02:34 +0100

    lab6 branch init

--- WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b82f6753330b9e29e6de4fa54e44021678c79681")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3db88482bc85d9f193957e0b7a0061216016f6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
